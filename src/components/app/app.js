cf396fbe1996339b274f3a0eb9595f7b93a9cdf7
Alex <merkulovwork@mail.ru>
2023-06-07 21:16:54 +1000

    Передача функий и их работа через пропсы. Переписал функциональный элемент на классы

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 
 import AppHeader from "../app-header";
 import SearchPanel from "../search-panel";
@@ -10,18 +10,51 @@ import PostAddForm from "../post-add-form/post-add-form";
 import './app.css'
 
 
-const App = () => {
-    return (
-        <div className="app">
-            <AppHeader/>
-            <div className="search-panel d-flex">
-                <SearchPanel/>
-                <PostStatusFilter/>
-            </div>
-            <PostList/>
-            <PostAddForm/>
-        </div>
-    )
-}
+export default class App extends Component{
 
-export default App;
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                2,
+                {label:'React', important:true, id:'qwer'},
+                {label:'Vue', important:false, id:'guth'},
+                {label:'Angular', important:false, id:'trtr'},
+            ]
+        }
+
+        this.deleteItem = this.deleteItem.bind(this)
+
+    }
+
+    deleteItem(id){
+        this.setState(({data})=>{
+            const index = data.findIndex(elem => elem.id === id)
+            const newData = [...data.slice(0, index), ...data.slice(index + 1)]
+            return{
+                data: newData
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <AppHeader/>
+                <div className="search-panel d-flex">
+                    <SearchPanel/>
+                    <PostStatusFilter/>
+                </div>
+                <PostList
+                    posts={this.state.data}
+                    onDelete={id=>{
+                        this.deleteItem(id)
+                    }
+                    }/>
+                <PostAddForm/>
+            </div>
+        )
+    }
+
+
+}
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -4,12 +4,25 @@ import './post-list.css'
 
 import PostListItem from "../post-list-item/post-list-item"
 
-const PostList = () => {
+const PostList = ({posts, onDelete}) => {
+
+    let elements = posts.map((item)=>{
+        if(typeof(item) == 'object'){
+            const {id, ...itemProps} = item
+            return(
+                <li key={id} className={'list-group-item'}>
+                    <PostListItem
+                        {...itemProps}
+                        onDelete={()=>onDelete(id)}/>
+                </li>
+            )
+        }
+        return null //если не объект, то вернуть просто null
+    })
+
   return(
       <ul className="app-list list-group">
-        <PostListItem/>
-        <PostListItem/>
-        <PostListItem/>
+          {elements}
       </ul>
   )
 }
